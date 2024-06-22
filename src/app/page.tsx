762a70cb640f079cb184be180cb009935ed20ab3
SanjanaVeeranna <sahana.v1405@gmail.com>
2024-06-22 19:44:48 +0530

    added the infite loop on + button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,47 +17,45 @@ export default function Home() {
   };
 
   return (
-    <div>
-      <div>
-        <div className="h-12 w-18 flex ">
-          <button
-            onClick={handleAdd}
-            className="bg-blue-500 text-white font-bold py-2 px-4 rounded-lg"
-          >
-            Click here
-          </button>
+    <div className="h-screen w-full flex items-center justify-center bg-gray-200">
+      <div className="h-12 w-18 flex ">
+        <button
+          onClick={handleAdd}
+          className="bg-blue-500 text-white font-bold py-2 px-4 rounded-lg"
+        >
+          Click here
+        </button>
 
-          {popUps.map((isPopUpVisible, index) =>
-            isPopUpVisible ? (
-              <motion.div
-                key={index}
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                exit={{ opacity: 0 }}
-                className="border fixed left-0 top-0 z-50 flex h-screen w-screen items-center justify-center bg-transparent bg-opacity-30"
-              >
-                <div className="flex h-56 w-56 flex-col items-center justify-center gap-y-4 rounded-lg bg-white">
-                  <div className="font-bold text-black">{`Hello World ${
-                    index + 1
-                  }`}</div>
+        {popUps.map((isPopUpVisible, index) =>
+          isPopUpVisible ? (
+            <motion.div
+              key={index}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              className="fixed left-0 top-0 z-50 flex h-screen w-screen items-center justify-center bg-transparent bg-opacity-30"
+            >
+              <div className="flex h-56 w-56 flex-col items-center justify-center gap-y-4 rounded-lg bg-white shadow-lg">
+                <div className="font-bold text-black">{`Hello World ${
+                  index + 1
+                }`}</div>
 
-                  <div
-                    onClick={() => handleClose(index)}
-                    className="flex h-10 w-36 items-center justify-center rounded-lg bg-gray-300 cursor-pointer"
-                  >
-                    Close
-                  </div>
-                  <div
-                    onClick={handleAdd}
-                    className="flex h-10 w-36 cursor-pointer items-center justify-center rounded-md bg-blue-500 text-base"
-                  >
-                    <Plus />
-                  </div>
+                <div
+                  onClick={() => handleClose(index)}
+                  className="flex h-10 w-36 items-center justify-center rounded-lg bg-gray-300 cursor-pointer"
+                >
+                  Close
                 </div>
-              </motion.div>
-            ) : null
-          )}
-        </div>
+                <div
+                  onClick={handleAdd}
+                  className="flex h-10 w-36 cursor-pointer items-center justify-center rounded-md bg-blue-500 text-base"
+                >
+                  <Plus />
+                </div>
+              </div>
+            </motion.div>
+          ) : null
+        )}
       </div>
     </div>
   );
